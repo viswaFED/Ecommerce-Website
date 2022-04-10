12320186b411cde3cbb3661e04d3e8286ff95bc5
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-04-10 18:38:30 +0530

    with packags uploaded

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,41 +1,46 @@
 import React, { useContext } from "react";
-import { Nav, NavLink, NavMenu,NavBtn, NavGen } from "./NavbarElements";
+import { Nav, NavLink, NavMenu, NavBtn, NavGen } from "./NavbarElements";
 import CartContext from "../Cart/CartContext";
 const Navbar = (props) => {
+  // class Counter extends Component {
+  //   state = {
+  //     flag: false,
+  //   };
+  // }
 
-//   class Counter extends Component {
-//   state = {
-//     flag: false
-//   };
-// }
-
-//   show = () => {
-//     if (this.state.flag === true) {
-//       return (
-//           <NavBtn onClick={props.Cart}>cart </NavBtn>
-//       );
-//     } 
-//   };
+  // show = () => {
+  //   if (this.state.flag === true) {
+  //     return <NavBtn onClick={props.Cart}>cart </NavBtn>;
+  //   }
+  // };
   //   onClick={() => { this.setState({ flag: true });}}
   let total = 0;
   const cartCntxt = useContext(CartContext);
   cartCntxt.items.forEach((element) => {
     total += element.quantity;
   });
-   const btn =  <NavBtn onClick={props.Cart}>cart ({total}) </NavBtn>
-// 
+  const btn =  <NavBtn onClick={props.Cart}>cart ({total}) </NavBtn>
+  //
   return (
     <header>
       <Nav>
         <NavMenu>
-          <NavLink to="/auth" >Login</NavLink>
+          <NavLink to="/auth">Login</NavLink>
           <NavLink to="/Home">Home</NavLink>
-          <NavLink to="/Store" >Store</NavLink>
+          <NavLink id="showcart"
+            to="/Store "
+            onClick={() => {
+              this.setState({ flag: true });
+            }}
+          >
+            Store
+          </NavLink>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/contact-us">Contact Us</NavLink>
           <NavLink to="/profile">Profile</NavLink>
         </NavMenu>
-        {/* {this.show()} */} {btn}
+        {/* {this.show()} */}
+        {btn}
       </Nav>
       <NavGen>
         <h1 style={{ fontFamily: "Times New Roman" }}>The Generics</h1>
